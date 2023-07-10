4f4d582b5cb4a741868ee38a731a49837c4b1f85
raj <interplaytheheads@gmail.com>
2023-07-10 15:07:14 +0300

    Initial Commit

---
+++ ServiceButtonBackend/Controllers/CharacterController.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ServiceButtonBackend.Controllers
+{
+    [ApiController]
+    [Route("api/v-1")]
+    public class CharacterController : ControllerBase
+    {
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("getall")]
+        
+        public ActionResult<Character> Get()
+        {
+            return Ok(_characterService.GetAllCharacters());
+        }
+
+        [HttpGet("getsingle")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(_characterService.GetCharacterById(id));
+        }
+
+        [HttpPost("character/add")]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+            return Ok(_characterService.AddCharacter(newCharacter));
+        }
+
+
+
+
+    }
+}
---
+++ ServiceButtonBackend/Services/CharacterService/CharacterService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using ServiceButtonBackend.Models;
+
+namespace ServiceButtonBackend.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private readonly List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character() {Id = 1, Name = "Sam"}
+        };
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            CharacterService.Add(newCharacter);
+            return characters;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            return characters.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
