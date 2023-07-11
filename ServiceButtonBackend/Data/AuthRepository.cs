ebc404c8bccdaf0be9554e0093082b522f6ac2f7
raj <interplaytheheads@gmail.com>
2023-07-11 16:15:24 +0300

    User Login Implemented

--- ServiceButtonBackend/Data/AuthRepository.cs
+++ ServiceButtonBackend/Data/AuthRepository.cs
@@ -8,11 +8,34 @@
         {
             _context = context;
         }
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<string>();
+            //check if user exist
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+
+            //check the user is not null
+            if (user is null) 
+            {
+                response.Success = false;
+                response.Message = "User not Found.";
+            }
+            //check the password is verified
+            else if(!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong Password.";
+            }
+            //Set the user data to response.Data
+            else
+            {
+                response.Data = user.Id.ToString();
+            }
+
+            return response;
         }
 
+        //Register User 
         public async Task<ServiceResponse<int>> Register(User user, string password)
         {
             var response = new ServiceResponse<int>();
@@ -36,6 +59,7 @@
             return response;
         }
 
+        //Check If user Already Exists
         public async Task<bool> UserExists(string username)
         {
             if(await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
@@ -45,6 +69,7 @@
             return false;
         }
 
+        //Create Password Hash and Salt
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
             using(var hmac = new System.Security.Cryptography.HMACSHA512())
@@ -53,5 +78,18 @@
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
+
+
+        //Verify Password Hash
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+
+
     }
 }
---
+++ ServiceButtonBackend/Dtos/User/UserLoginDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceButtonBackend.Dtos.User
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
