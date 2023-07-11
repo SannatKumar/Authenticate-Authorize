93d05e69c26f5a02e4d59b5697a4a59eaffa5530
raj <interplaytheheads@gmail.com>
2023-07-11 14:23:48 +0300

    Get Data Working

--- ServiceButtonBackend/Services/CharacterService/CharacterService.cs
+++ ServiceButtonBackend/Services/CharacterService/CharacterService.cs
@@ -11,12 +11,16 @@ namespace ServiceButtonBackend.Services.CharacterService
             new Character(),
             new Character() {Id = 1, Name = "Sam"}
         };
-        private readonly IMapper _mapper;
 
-        public CharacterService(IMapper mapper)
+        //For AutoMapper
+        private readonly IMapper _mapper;
+        //For Data Context
+        private readonly DataContext _context;
+
+        public CharacterService(IMapper mapper, DataContext context)
         {
             _mapper = mapper;
-            
+            _context = context;
         }
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
@@ -56,7 +60,8 @@ namespace ServiceButtonBackend.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
 
@@ -65,9 +70,9 @@ namespace ServiceButtonBackend.Services.CharacterService
         {
             //return characters.FirstOrDefault(c => c.Id == id)!;
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
 
-            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
 
             return serviceResponse;
 
