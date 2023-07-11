ed0aaa0eac4de47be9e54a2c93bf46d0e63c9125
raj <interplaytheheads@gmail.com>
2023-07-11 15:36:50 +0300

    UserExists Checking Function

--- ServiceButtonBackend/Models/User.cs
+++ ServiceButtonBackend/Models/User.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public string UserName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
 
         public byte[] PasswordHash { get; set; } = new byte[0];
 
